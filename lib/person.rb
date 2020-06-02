84454e6b2e072ad37570fb51287fafb93d710dd9
elsa chen <snowing222@gmail.com>
2020-06-02 15:38:07 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,6 +3,8 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-def initialize()
+def initialize(attributes)
+  attribute.each {|key,value| self.send(("#{key=}"),value)}
+end
   #your code here
 end
